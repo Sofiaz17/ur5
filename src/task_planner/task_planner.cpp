ceb5a7af1f5cf3cc11d00588593176ccb929d4c2
Sofiaz17 <sofia.zandona09@gmail.com>
2024-06-22 15:32:34 +0200

    tried manually passing coordinates [NOT WORKING]

--- src/task_planner/task_planner.cpp
+++ src/task_planner/task_planner.cpp
@@ -11,11 +11,27 @@
 #include "ur5/BoundingBox.h"
 #include "ur5/BlockParams.h"
 
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Geometry> 
+
 #include <vector>
 
+using namespace Eigen;
 
 char info_name[] = " [  task_planner  ]:";
 bool debug_mode = false;
+
+bool waitForServiceResponse(ros::ServiceClient &client, ur5::GetBrickPose &srv, int retries = 10, int delay_ms = 1000) {
+    for (int i = 0; i < retries; ++i) {
+        if (client.call(srv)) {
+            return true;
+        }
+        ROS_WARN("%s Waiting for vision node to provide data...", info_name);
+        ros::Duration(delay_ms / 1000.0).sleep();
+    }
+    return false;
+}
 
 
 
@@ -43,20 +59,16 @@ int main(int argc, char **argv)
 	std::vector<ur5::BlockParams> actual_poses;
 
 	// ros::ServiceClient vision_node1 = node.serviceClient<ur5::GetBoundingBoxes>("get_bounding_boxes");
-	ros::ServiceClient vision_node = node.serviceClient<ur5::GetBrickPose>("get_brick_pose");
-	// ros::ServiceClient pointcloud_node = node.serviceClient<ur5::BoundingBoxesToPoses>("bounding_boxes_to_poses");
+	// ros::ServiceClient vision_node = node.serviceClient<ur5::GetBrickPose>("get_brick_pose");
 	ros::ServiceClient desired_poses_node = node.serviceClient<ur5::GetDesiredPoses>("get_desired_poses");
 	ros::ServiceClient motion_planner = node.serviceClient<ur5::MoveBlock>("move_block");
-	vision_node.waitForExistence();
-	//pointcloud_node.waitForExistence();
+	// vision_node.waitForExistence();
 	desired_poses_node.waitForExistence();
 	motion_planner.waitForExistence();
 
-	//ur5::GetBoundingBoxes get_bounding_boxes_service;
-	ur5::GetBrickPose get_brick_pose_service;
-	//ur5::BoundingBoxesToPoses bounding_boxes_to_poses_service;
+	// ur5::GetBrickPose get_brick_pose_service;
 	ur5::GetDesiredPoses get_desired_poses_service;
-	// ur5::MoveBlock move_block_service;
+	ur5::MoveBlock move_block_service;
 
 	ros::param::get("/debug_mode", debug_mode);
 	ROS_INFO("%s task_planner is ready!", info_name);
@@ -79,37 +91,65 @@ int main(int argc, char **argv)
 	// 	ROS_ERROR("%s Failed to call service get_bounding_boxes", info_name);
 	// 	return 1;
 	// }
-	service_exit = vision_node.call(get_brick_pose_service);
+	//service_exit = vision_node.call(get_brick_pose_service);
+	//  service_exit = waitForServiceResponse(vision_node, get_brick_pose_service);
+    // if (!service_exit) {
+    //     ROS_ERROR("%s Failed to call service get_brick_pose_service", info_name);
+    //     return 1;
+    // } else {
+    //     ROS_INFO("get_brick_pose_service obtained!");
+    // }
+
 	
-	if(!service_exit){
-		ROS_ERROR("%s Failed to call service get_brick_pose_service", info_name);
-		return 1;
-	} else {
-		ROS_INFO("get_brick_pose_service obtained!");
-	}
-
-	/// Pointcloud Node
-
-	// if(debug_mode) ROS_INFO("%s Pointcloud phase", info_name);
-
-	// bounding_boxes_to_poses_service.request.dim = get_bounding_boxes_service.response.dim;
-	// bounding_boxes_to_poses_service.request.boxes = get_bounding_boxes_service.response.boxes;
-
-	// service_exit = pointcloud_node.call(bounding_boxes_to_poses_service);
 	// if(!service_exit){
-	// 	ROS_ERROR("%s Failed to call service bounding_boxes_to_poses_service", info_name);
+	// 	ROS_ERROR("%s Failed to call service get_brick_pose_service", info_name);
 	// 	return 1;
+	// } else {
+	// 	ROS_INFO("get_brick_pose_service obtained!");
 	// }
 
-	actual_poses = get_brick_pose_service.response.poses; //pos blocchi visti da vision
 
+	//actual_poses = get_brick_pose_service.response.poses; //pos blocchi visti da vision
+
+
+//Roll pitch and yaw in Radians
+float roll = 0, pitch = 0, yaw = 0.5236;    
+Quaternionf q;
+q = AngleAxisf(roll, Vector3f::UnitX())
+    * AngleAxisf(pitch, Vector3f::UnitY())
+    * AngleAxisf(yaw, Vector3f::UnitZ());
+std::cout << "Quaternion" << std::endl << q.coeffs() << std::endl;
+
+	
+  	// ur5::BlockParams block;
+    // block.label = "X1-Y4-Z2";
+    // block.pose.position.x = 0.4;
+    // block.pose.position.y = 0.4;
+    // block.pose.position.z = 0.87;
+    // block.pose.orientation.x = q.x();
+    // block.pose.orientation.y = q.y();
+    // block.pose.orientation.z = q.z();
+    // block.pose.orientation.w = q.w();
+
+    // actual_poses.push_back(block);
+
+
+	actual_poses.push_back(ur5::BlockParams());
+	actual_poses.at(0).label = "X1-Y1-Z2";
+	actual_poses.at(0).pose.position.x = 0.4;
+	actual_poses.at(0).pose.position.y = 0.4;
+	actual_poses.at(0).pose.position.z = 0.87;
+	actual_poses.at(0).pose.orientation.x = q.x();
+	actual_poses.at(0).pose.orientation.y = q.y();
+	actual_poses.at(0).pose.orientation.z = q.z();
+	actual_poses.at(0).pose.orientation.w = q.w();
 
 	/// Desired Poses Node
 
 	if(debug_mode) ROS_INFO("%s Retrieving desired poses", info_name);
 	ROS_INFO("%s Retrieving desired poses", info_name);
 
-	// get_esired_poses_service.request is empty
+	// get_desired_poses_service.request is empty
 
 	service_exit = desired_poses_node.call(get_desired_poses_service);
 	if(!service_exit){
@@ -157,10 +197,10 @@ ur5::BlockParams actual_block;
 		} else {
 			actual_block = *it;
 
-			// move_block_service.request.start_pose = actual_block;
-			// move_block_service.request.end_pose = desired_block;
+			move_block_service.request.start_pose = actual_block;
+			move_block_service.request.end_pose = desired_block;
 
-			// service_exit = motion_planner.call(move_block_service);
+			service_exit = motion_planner.call(move_block_service);
 			if(!service_exit){
 				ROS_ERROR("%s Failed to call service move_block", info_name);
 				return 1;
@@ -168,9 +208,9 @@ ur5::BlockParams actual_block;
 				ROS_INFO("move_block_service obtained!");
 			}
 
-			// if(!move_block_service.response.success){
-			// 	ROS_WARN("%s motion_planner failed to move block \"%s\"", info_name, desired_block.label.c_str());
-			// } 
+			if(!move_block_service.response.success){
+				ROS_WARN("%s motion_planner failed to move block \"%s\"", info_name, desired_block.label.c_str());
+			} 
 			actual_poses.erase(it);
 		}
 	}
